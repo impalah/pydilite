23138a1f1089f7c87ede9e0d6e534ba12aa8f028
impalah <impalah@gmail.com>
2024-03-23 21:59:58 +0100

    First upload

---
+++ README.build.md
@@ -0,0 +1,22 @@
+# pydilite
+
+Python Lightweight dependency injection package.
+
+Strongly based on Pythondi: https://github.com/teamhide/pythondi
+
+## Requirements
+
+- Python >= 3.12
+- Poetry
+
+## Install
+
+```bash
+poetry install
+```
+
+## Run unit tests
+
+```bash
+poetry run pytest tests
+```
---
+++ tests/test_configure.py
@@ -0,0 +1,67 @@
+from pytest import raises
+
+from pydilite import (
+    configure,
+    configure_after_clear,
+    Provider,
+    Container,
+)
+from pydilite.exceptions import AlreadyInjectedException
+
+
+class Repo:
+    def __init__(self): ...
+
+
+class SQLRepo:
+    def __init__(self): ...
+
+
+def test_configure_lazy_is_true():
+    Container.clear()
+    provider = Provider()
+    provider.bind(Repo, SQLRepo, lazy=True)
+    configure(provider=provider)
+    assert isinstance(Container.get(), Provider)
+    assert Container.get().bindings[Repo] == SQLRepo
+    with raises(AlreadyInjectedException):
+        configure(provider=provider)
+    Container.clear()
+
+
+def test_configure_lazy_is_false():
+    Container.clear()
+    provider = Provider()
+    provider.bind(Repo, SQLRepo, lazy=False)
+    configure(provider=provider)
+    assert isinstance(Container.get(), Provider)
+    assert Container.get().bindings[Repo] != SQLRepo
+    with raises(AlreadyInjectedException):
+        configure(provider=provider)
+    Container.clear()
+
+
+def test_configure_after_clear_lazy_is_true():
+    provider = Provider()
+    provider.bind(Repo, SQLRepo, lazy=True)
+    configure_after_clear(provider=provider)
+
+    provider = Provider()
+    provider.bind(SQLRepo, Repo, lazy=True)
+    configure_after_clear(provider=provider)
+    with raises(KeyError):
+        assert isinstance(Container.get(), Provider)
+        assert Container.get().bindings[Repo] == SQLRepo
+
+
+def test_configure_after_clear_lazy_is_false():
+    provider = Provider()
+    provider.bind(Repo, SQLRepo, lazy=False)
+    configure_after_clear(provider=provider)
+
+    provider = Provider()
+    provider.bind(SQLRepo, Repo, lazy=False)
+    configure_after_clear(provider=provider)
+    with raises(KeyError):
+        assert isinstance(Container.get(), Provider)
+        assert Container.get().bindings[Repo] != SQLRepo
